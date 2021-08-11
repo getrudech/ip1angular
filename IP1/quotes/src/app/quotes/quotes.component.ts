d8ab93497867c3b72227f568b9c62c9bc82ba4b4
getty <gcherono15@gmail.com>
2021-08-11 11:20:04 +0300

    created a component called quotes

---
+++ IP1/quotes/src/app/quotes/quotes.component.ts
@@ -0,0 +1,23 @@
+import { Quote } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+
+  quotes:quote[] = [
+    {id:1, name:'Watch finding Nemo'},
+    {id:2,name:'Buy Cookies'},
+    {id:3,name:'Get new Phone Case'},
+    {id:4,name:'Get Dog Food'},
+    {id:5,name:'Solve math homework'},
+    {id:6,name:'Plot my world domination plan'},
+  ];
+
+  ngOnInit(): void {
+  }
+
+}
