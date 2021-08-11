5901fff8f4d4a044665176da157e93bcda865e7e
getty <gcherono15@gmail.com>
2021-08-11 14:07:20 +0300

    changed the name of the author

--- IP1/quotes/src/app/quotes/quotes.component.ts
+++ IP1/quotes/src/app/quotes/quotes.component.ts
@@ -10,8 +10,8 @@ export class QuotesComponent implements OnInit {
 
   quotes:Quotes[] = [
     new Quotes(1, 'Let your light shine', 'Fin bygo'),
-    new Quotes(2,'Buy Cookies','I have to buy cookies for the parrot'),
-    new Quotes(3,'Get new Phone Case','Diana has her birthday coming up soon'),
+    new Quotes(2,'We can, we will and we must','Liz Ngure'),
+    new Quotes(3,'Let your heartbelieve it and you will achieve it','Nimo Banks'),
     new Quotes(4,'Get Dog Food','Pupper likes expensive snacks'),
     new Quotes(5,'Solve math homework','Damn Math'),
     new Quotes(6,'Plot my world domination plan','Cause I am an evil overlord'),
